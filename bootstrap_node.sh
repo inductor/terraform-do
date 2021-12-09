b29d533d6939fdc601a1de6d69c58c5ac89b7e58
inductor(Kohei) <kela@inductor.me>
2021-12-09 10:58:39 +0900

    Update bootstrap_node.sh

--- bootstrap_node.sh
+++ bootstrap_node.sh
@@ -39,6 +39,8 @@ else
   sed -i -e "s/SystemdCgroup \= false/SystemdCgroup \= true/g" /etc/containerd/config.toml
 fi
 
+sudo systemctl restart containerd
+
 cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
